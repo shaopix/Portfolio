6d7b77ef0616917f701848560044f642694e2717
Shaopix <varta@live.fr>
2023-09-29 09:22:13 +0200

    refonte menu

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -1,26 +1,32 @@
 <template>
   <div id="header">
     <img src="../assets/logo_portfolio.png"/>
+    <MainMenu></MainMenu>
   </div>
 </template>
 <script lang="ts">
 import { defineComponent } from "vue";
-import type { PropType } from "vue";
-import MenuSection from "@/types/MenuSection";
+import MainMenu from "../components/MainMenu.vue";
 
 export default defineComponent({
-  name: "MainMenu",
+  name: "HeaderApp",
   methods: {},
-  setup() {
-    return {};
-  },
+  components:{
+    MainMenu
+  }
 });
 </script>
 <style scoped lang="scss">
+@font-face {
+  font-family: "Bukhari Script";
+  src: url('../assets/fonts/Bukhari\ Script.ttf');
+}
 #header {
   background-color: #1f203a;
   min-height: 100px;
   width: 100%;
+  font-family: Bukhari Script;
+  display: flex;
   img{
     width: 183px;
   }
---
+++ src/components/MainMenu.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="menu">
+    <div class="menu__link">Acceuil</div>
+    <summary class="menu__link">Exemple</summary>
+    <div class="menu__link">Contact</div>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "MainMenu",
+  methods: {},
+  setup() {
+    return {};
+  },
+});
+</script>
+<style scoped lang="scss">
+.menu{
+  display: flex;
+  justify-content: space-evenly;
+  width: 100%;
+  margin: auto;
+  .menu__link{
+    font-size: 25px;
+    height: 100%;
+    color: #f6f3e4;
+    cursor: pointer;
+  }
+}
+</style>
